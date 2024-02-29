2dbced99a693852630f4d78a093b4c484e245de2
FatemeGhaderiz <ghaderifateme131@gmail.com>
2024-02-29 13:40:05 +0330

    second commit

--- shift/selectors/interference.py
+++ shift/selectors/interference.py
@@ -1,7 +1,31 @@
 from django.db.models import Q
-
+import datetime
 from driver_vehicle_app.models import Appointment
 
+
+def check_monthly_weekly_days(month_days, week_days, start_date , end_date):
+     current_date = start_date
+     while current_date <= end_date:
+        if current_date.day in month_days:
+            if current_date.weekday() in week_days:
+                return False
+        current_date += datetime.timedelta(days=1)
+     return True    
+
+
+
+
+def check_weekly_monthly_days( week_days, month_days, start_date , end_date):
+     current_date = start_date
+     while current_date <= end_date:
+        if current_date.weekday() in week_days:
+            if current_date.day in month_days:
+                return False
+        current_date += datetime.timedelta(days=1)
+     return True  
+
+
+# //////////////////////////////////////////////////////////////////////
 
 def check_driver_car_daily_daily_shift(id, start_date, end_date, start_time, end_time):
     appointment = Appointment.objects.get(id = id)
@@ -97,6 +121,35 @@ def check_driver_car_weekly_weekly_shift(id, start_date, end_date, start_time, e
 
 
 
+
+def check_driver_car_weekly_monthly_shift(id, start_date, end_date, start_time, end_time, weekdays):
+    appointment = Appointment.objects.get(id = id)
+
+    if appointment.monthly_shift.filter(Q((Q(date_start__lte=start_date) & Q(date_end__gte=start_date)) 
+                                      | Q(Q(date_start__lte=end_date) & Q(date_end__gte=end_date))
+                                      | Q(Q(date_start__gte=start_date) & Q(date_end__lte=end_date)))
+                                      & Q(Q(Q(time_start__lte=start_time) & Q(time_end__gte=start_time)) 
+                                      | Q(Q(time_start__lte=end_time) & Q(time_end__gte=end_time))
+                                      | Q(Q(time_start__gte=start_time) & Q(time_end__lte=end_time)))).exists():
+    
+        shift = appointment.monthly_shift.filter(Q((Q(date_start__lte=start_date) & Q(date_end__gte=start_date)) 
+                                      | Q(Q(date_start__lte=end_date) & Q(date_end__gte=end_date))
+                                      | Q(Q(date_start__gte=start_date) & Q(date_end__lte=end_date)))
+                                      & Q(Q(Q(time_start__lte=start_time) & Q(time_end__gte=start_time)) 
+                                      | Q(Q(time_start__lte=end_time) & Q(time_end__gte=end_time))
+                                      | Q(Q(time_start__gte=start_time) & Q(time_end__lte=end_time))))
+        
+        for obj in shift:
+            li = list(obj.days.split(","))
+            if check_weekly_monthly_days(weekdays,li,start_date,end_date):
+                return False
+
+       
+    return True   
+
+
+
+
 # //////////////////////////////////////////////
 
 
@@ -112,6 +165,35 @@ def check_driver_car_monthly_daily_shift(id, start_date, end_date, start_time, e
     
         return False
     return True    
+
+
+
+
+def check_driver_car_monthly_weekly_shift(id, start_date, end_date, start_time, end_time, month_days):
+    appointment = Appointment.objects.get(id = id)
+
+    if appointment.weekly_shift.filter(Q((Q(date_start__lte=start_date) & Q(date_end__gte=start_date)) 
+                                      | Q(Q(date_start__lte=end_date) & Q(date_end__gte=end_date))
+                                      | Q(Q(date_start__gte=start_date) & Q(date_end__lte=end_date)))
+                                      & Q(Q(Q(time_start__lte=start_time) & Q(time_end__gte=start_time)) 
+                                      | Q(Q(time_start__lte=end_time) & Q(time_end__gte=end_time))
+                                      | Q(Q(time_start__gte=start_time) & Q(time_end__lte=end_time)))).exists():
+    
+        shift = appointment.weekly_shift.filter(Q((Q(date_start__lte=start_date) & Q(date_end__gte=start_date)) 
+                                      | Q(Q(date_start__lte=end_date) & Q(date_end__gte=end_date))
+                                      | Q(Q(date_start__gte=start_date) & Q(date_end__lte=end_date)))
+                                      & Q(Q(Q(time_start__lte=start_time) & Q(time_end__gte=start_time)) 
+                                      | Q(Q(time_start__lte=end_time) & Q(time_end__gte=end_time))
+                                      | Q(Q(time_start__gte=start_time) & Q(time_end__lte=end_time))))
+        
+        for obj in shift:
+            li = list(obj.days.split(","))
+            if check_monthly_weekly_days(month_days,li,start_date,end_date):
+                return False
+
+       
+    return True   
+ 
 
 
 
@@ -143,3 +225,8 @@ def check_driver_car_monthly_monthly_shift(id, start_date, end_date, start_time,
                         return False
        
     return True   
+
+
+
+
+
---
+++ shift/selectors/interference_group.py
@@ -0,0 +1,81 @@
+
+from django.db.models import Q
+
+from driver_vehicle_app.models import DriverGroup, DriversGroup, Driver, Appointment
+from .interference import check_driver_car_daily_daily_shift,check_driver_car_daily_weekly_shift,check_driver_car_daily_monthly_shift 
+from .interference import check_driver_car_weekly_daily_shift,check_driver_car_weekly_weekly_shift,check_driver_car_weekly_monthly_shift 
+from .interference import check_driver_car_monthly_daily_shift,check_driver_car_monthly_weekly_shift,check_driver_car_monthly_monthly_shift 
+
+
+def find_drivers_of_group(id):
+    drivers_id = []
+    group = DriverGroup.objects.get(id=id)
+
+    driver_group = DriversGroup.objects.filter(groupe = group)
+    for item in driver_group :
+        drivers_id.append(item.driver.id)
+
+    return drivers_id    
+
+
+
+def find_drivers_appoinment(id,start_date,end_date):
+    driver = Driver.objects.get(id=id)
+    object = Appointment.objects.get(((Q(date_start__lte=start_date) & Q(date_end__gte=start_date)) 
+                                      | Q(Q(date_start__lte=end_date) & Q(date_end__gte=end_date))
+                                      | Q(Q(date_start__gte=start_date) & Q(date_end__lte=end_date)))) 
+    return object.id    
+
+
+
+
+
+def check_driver_groupe_daily_shift(id, start_date, end_date, start_time, end_time):
+
+    drivers_id = find_drivers_of_group(id)
+
+    for driver in drivers_id:
+        appointment = find_drivers_appoinment(driver,start_date,end_date)
+        if (check_driver_car_daily_daily_shift(appointment,start_date,end_date,start_time,end_time) or
+           check_driver_car_daily_weekly_shift(appointment,start_date,end_date,start_time,end_time) or
+           check_driver_car_daily_monthly_shift(appointment,start_date,end_date,start_time,end_time)):
+            return False
+
+    return True       
+
+
+
+
+def check_driver_groupe_weekly_shift(id, start_date, end_date, start_time, end_time, days):
+
+    drivers_id = find_drivers_of_group(id)
+
+    for driver in drivers_id:
+        appointment = find_drivers_appoinment(driver,start_date,end_date)
+        if (check_driver_car_weekly_daily_shift(appointment,start_date,end_date,start_time,end_time, days) or
+           check_driver_car_weekly_weekly_shift(appointment,start_date,end_date,start_time,end_time, days) or
+           check_driver_car_weekly_monthly_shift(appointment,start_date,end_date,start_time,end_time, days)):
+            return False
+
+    return True       
+
+
+
+
+
+def check_driver_groupe_weekly_shift(id, start_date, end_date, start_time, end_time, days):
+
+    drivers_id = find_drivers_of_group(id)
+
+    for driver in drivers_id:
+        appointment = find_drivers_appoinment(driver,start_date,end_date)
+        if (check_driver_car_monthly_daily_shift(appointment,start_date,end_date,start_time,end_time, days) or
+           check_driver_car_monthly_weekly_shift(appointment,start_date,end_date,start_time,end_time, days) or
+           check_driver_car_monthly_monthly_shift(appointment,start_date,end_date,start_time,end_time, days)):
+            return False
+
+    return True       
+
+
+# ////////////////////////////////////////////////////////////////
+
